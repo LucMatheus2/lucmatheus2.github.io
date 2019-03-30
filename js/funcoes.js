016495203512d360dade46dfdeb62b8ee7305360
Lucas Matheus Costa <44246341+LucasMatheusCosta@users.noreply.github.com>
2019-03-30 00:20:23 -0300

    Delete funcoes.js

--- js/funcoes.js
+++
@@ -1,9 +0,0 @@
-function BotaoAcima(){
-	document.getElementById("SL_B").src="media/SL_Botao2.png";
-}
-function BotaoNormal(){
-	document.getElementById("SL_B").src="media/SL_Botao.png";
-}
-function link(){
-	document.getElementById("SL_B").src="media/SL_Botao3.png";
-}
